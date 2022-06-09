39a4df98b4ba2d98aa7712bc0a45552b9d846b08
Thara Mohanan <tharamohanan7@gmail.com>
2022-06-10 01:43:57 +0530

    feat: setup

---
+++ system/Router.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace System;
+
+use Exception;
+
+class Router
+{
+    /**
+     * Associastive array of routes
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * Parameter from matched routes
+     * @var array
+     */
+    protected $params = [];
+
+
+    public function add(String $route, array $params = []): void
+    {
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // Convert variables with custom regular expressions e.g. {id:\d+}
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+
+        $this->routes[$route] = $params;
+    }
+
+    public function getRoutes(): array
+    {
+        return $this->routes;
+    }
+
+    public function match(String $url): bool
+    {
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+
+    public function dispatch(String $url): void
+    {
+        $url = $this->removeQueryStringVariables($url);
+
+        if ($this->match($url)) {
+            $controller = $this->params['controller'];
+            $controller = $this->convertToStudlyCaps($controller);
+            $controller = $this->getNamespace() . $controller;
+
+            if (class_exists($controller)) {
+
+                $controller_object = new $controller($this->params);
+
+                $action =  $this->params['action'];
+
+                $action = $this->convertToCamelCase($action);
+
+                if (is_callable([$controller_object, $action])) {
+                    $controller_object->$action();
+                } else {
+                    throw new \Exception("Method $action (in controller $controller) not found");
+                }
+            } else {
+
+                throw new \Exception("Controller class $controller not found");
+            }
+        } else {
+
+            throw new \Exception('No route matched.', 404);
+        }
+    }
+
+    protected function convertToStudlyCaps(String $string): string
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+    }
+
+
+    private function convertToCamelCase(String $string): string
+    {
+        return lcfirst($this->convertToStudlyCaps($string));
+    }
+
+
+    protected function removeQueryStringVariables(String $url): string
+    {
+        if ($url != '') {
+            $parts = explode('&', $url, 2);
+
+            if (strpos($parts[0], '=') === false) {
+                $url = $parts[0];
+            } else {
+                $url = '';
+            }
+        }
+
+        return $url;
+    }
+
+    public function getNamespace(): string
+    {
+        $namespace = 'App\Controllers\\';
+
+        if (array_key_exists('namespace', $this->params)) {
+            $namespace .= $this->params['namespace'] . '\\';
+        }
+
+        return $namespace;
+    }
+}
